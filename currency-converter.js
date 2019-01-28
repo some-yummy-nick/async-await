054b47327afa11b0a17275c354371ac95b08049c
Marat Shaimardanov <marat.shaimardanov@quickex.io>
2019-01-28 17:01:16 +0300

    Add 2 async await requests

--- currency-converter.js
+++ currency-converter.js
@@ -15,13 +15,27 @@ const getExchangeRate = async (fromCurrency, toCurrency) => {
     }
 };
 
-const convertCurrency = async (fromCurrency, toCurrency, amount) => {
-    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
-    const convertedAmount = (amount * exchangeRate).toFixed(2);
-    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}.`;
+const getCountries = async (currencyCode) => {
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+
+        return response.data.map(country => country.name);
+    } catch (error) {
+        throw new Error(`Unable to get countries that use ${currencyCode}`);
+    }
 };
 
-convertCurrency('EUR', 'RUB', 20)
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+    const [exchangeRate, countries] = await Promise.all([
+        getExchangeRate(fromCurrency, toCurrency),
+        getCountries(toCurrency),
+    ]);
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+};
+
+convertCurrency('BTC', 'USD', 1)
     .then((message) => {
         console.log(message);
     }).catch((error) => {
